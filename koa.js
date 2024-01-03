54d03d42163e42eae84ee7640edba5b715139328
Kalkidan <kal05627356@gmail.com>
2024-01-03 11:18:05 -0800

    unsaved changes

--- koa.js
+++ koa.js
@@ -1,12 +1,15 @@
 const Koa = require('koa');
 const mongoose = require('./db_connection.js');
 const userController = require('./controllers/user.controller.js')
+const authMiddleware = reuire('./middlewares/auth.middlware')
+
 const Router = require('koa-router');
 
 const app = new Koa();
 const router = new Router();
 
-router.get('/cron', userController)
+// authMiddleware creatadded while I was in the call and added here after the call is closed since I fogot to add it in the call :)
+router.get('/cron',userMiddleware, userController)
 
 
 app.use(async (ctx, next) => {
